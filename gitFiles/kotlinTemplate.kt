027716dd3864cabffa6182a9476007cb8fa9d28b
Astron_n <60102228+AstronN-git@users.noreply.github.com>
2021-09-30 15:29:15 +0300

    Update kotlinTemplate.kt

--- gitFiles/kotlinTemplate.kt
+++ gitFiles/kotlinTemplate.kt
@@ -9,16 +9,24 @@ import java.util.*
 import java.io.*
 
 fun main() {
-    val input = Input()
-    solve(input)
+    solve()
     output.flush()
 }
 
 val output = PrintWriter(BufferedOutputStream(System.out))
+val input = Input()
 
-fun solve(input : Input) {
+fun solve() {
     
 }
+
+fun _println(a : Any) = output.println(a)
+fun _iprintln(a : Any) {output.println(a); output.flush()}
+
+fun _readint() : Int = input.nextInt()
+fun _readlong() : Long = input.nextLong()
+fun _nextline() : String = input.nextLine()
+fun _nextstring() : String = input.next()
 
 class Input {
     private val br = BufferedReader(InputStreamReader(System.`in`))
