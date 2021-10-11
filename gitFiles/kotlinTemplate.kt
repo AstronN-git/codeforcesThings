d0dbb891d46ddb10fbd3d92b72db1eff2e831362
Astron_n <60102228+AstronN-git@users.noreply.github.com>
2021-10-11 07:58:42 +0300

    Update kotlinTemplate.kt

--- gitFiles/kotlinTemplate.kt
+++ gitFiles/kotlinTemplate.kt
@@ -7,19 +7,25 @@
 
 import java.util.*
 import java.io.*
+import kotlin.collections.HashMap
 import kotlin.math.*
 
+const val DEBUG = true
+
+var output : PrintWriter = PrintWriter(BufferedOutputStream(System.out))
+
 fun main() {
+    if (DEBUG) output = PrintWriter(BufferedOutputStream(FileOutputStream("out.log")))
+
     solve()
     output.flush()
 }
 
 fun solve() {
-
+    
 }
 
-val output = PrintWriter(BufferedOutputStream(System.out))
-val input = Input()
+val input = Input(DEBUG)
 
 fun _println(a : Any) = output.println(a)
 fun _println() = output.println()
@@ -31,12 +37,16 @@ fun _nextlong() : Long = input.nextLong()
 fun _nextline() : String = input.nextLine()
 fun _nextstring() : String = input.next()
 
+fun IntArray.read() : IntArray {for (i in this.indices) this[i] = _nextint(); return this}
+fun LongArray.read() : LongArray {for (i in this.indices) this[i] = _nextlong(); return this}
+
 fun _nextints() : ArrayList<Int> = ArrayList(_nextline().split(" ").map {it -> it.toInt()})
 fun _nextlongs() : ArrayList<Long> = ArrayList(_nextline().split(" ").map {it -> it.toLong()})
 fun _nextstrings() : ArrayList<String> = ArrayList(_nextline().split(" "))
 
-class Input {
-    private val br = BufferedReader(InputStreamReader(System.`in`))
+class Input (DEBUG : Boolean) {
+
+    private val br = if (DEBUG) BufferedReader(InputStreamReader(FileInputStream("in.log"))) else BufferedReader(InputStreamReader(System.`in`))
     private var st : StringTokenizer = StringTokenizer("")
 
     fun next() : String {
