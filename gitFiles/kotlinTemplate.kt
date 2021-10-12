e6f5953f23b35ed76d9648b63289ae14457168c1
Astron_n <60102228+AstronN-git@users.noreply.github.com>
2021-10-12 15:21:29 +0300

    Update kotlinTemplate.kt

--- gitFiles/kotlinTemplate.kt
+++ gitFiles/kotlinTemplate.kt
@@ -7,36 +7,32 @@
 
 import java.util.*
 import java.io.*
-import kotlin.collections.HashMap
 import kotlin.math.*
 
 const val DEBUG = false
 
 @kotlin.ExperimentalStdlibApi
 fun main() {
-    if (DEBUG) output = PrintWriter(BufferedOutputStream(FileOutputStream("out.log")))
-
     solve()
     output.flush()
 }
 
 @kotlin.ExperimentalStdlibApi
 fun solve() {
-    
+
 }
 
-val input = Input(DEBUG)
-var output : PrintWriter = PrintWriter(BufferedOutputStream(System.out))
+var output : PrintWriter = PrintWriter(BufferedOutputStream(if (DEBUG) FileOutputStream("out.log") else System.out))
 
 fun _println(a : Any) = output.println(a)
 fun _println() = output.println()
 fun _print(a : Any) = output.print(a)
 fun _iprintln(a : Any) {output.println(a); output.flush()}
 
-fun _nextint() : Int = input.nextInt()
-fun _nextlong() : Long = input.nextLong()
-fun _nextline() : String = input.nextLine()
-fun _nextstring() : String = input.next()
+fun _nextint() : Int = _nextstring().toInt()
+fun _nextlong() : Long = _nextstring().toLong()
+fun _nextline() : String = br.readLine() ?: ""
+fun _nextstring() : String { while (st.hasMoreElements().not()) st = StringTokenizer(br.readLine() ?: return "", " "); return st.nextToken() }
 
 fun IntArray.read() : IntArray {for (i in this.indices) this[i] = _nextint(); return this}
 fun LongArray.read() : LongArray {for (i in this.indices) this[i] = _nextlong(); return this}
@@ -47,16 +43,5 @@ fun _nextints() : ArrayList<Int> = ArrayList(_nextline().split(" ").map {it -> i
 fun _nextlongs() : ArrayList<Long> = ArrayList(_nextline().split(" ").map {it -> it.toLong()})
 fun _nextstrings() : ArrayList<String> = ArrayList(_nextline().split(" "))
 
-class Input (DEBUG : Boolean) {
-    private val br = if (DEBUG) BufferedReader(InputStreamReader(FileInputStream("in.log"))) else BufferedReader(InputStreamReader(System.`in`))
-    private var st : StringTokenizer = StringTokenizer("")
-
-    fun next() : String {
-        while (st.hasMoreElements().not()) st = StringTokenizer(br.readLine() ?: return "", " ")
-        return st.nextToken()
-    }
-
-    fun nextInt() : Int = next().toInt()
-    fun nextLong() : Long = next().toLong()
-    fun nextLine() : String = br.readLine() ?: ""
-}
+val br = BufferedReader(InputStreamReader(if (DEBUG) FileInputStream("in.log") else System.`in`))
+var st = StringTokenizer("")
