4ad152a53e2f73b30942e07e921e130361278b97
Astron_n <60102228+AstronN-git@users.noreply.github.com>
2021-10-24 11:38:24 +0300

    Update kotlinTemplate.kt

--- gitFiles/kotlinTemplate.kt
+++ gitFiles/kotlinTemplate.kt
@@ -1,28 +1,36 @@
-/*
-    template: https://github.com/AstronN-git/codeforcesThings/blob/main/gitFiles/kotlinTemplate.kt
-    author: _Astron
-    for codeforces
-    file creation date: ${DATE}-${HOUR}:${MINUTE}:${SECOND}
-*/
-
 import java.util.*
 import java.io.*
+import kotlin.collections.*
 import kotlin.math.*
 
-const val DEBUG = false
+const val FILE_IN_OUT = false
+const val FILE_IN = "input.txt"
+const val FILE_OUT = "output.txt"
+
+const val TESTS = true
 
 @kotlin.ExperimentalStdlibApi
 fun main() {
-    solve()
+    if (TESTS) {
+        presolve()
+        var t = _nextint()
+        while (t-- > 0) solve()
+    } else
+        solve()
     output.flush()
 }
 
 @kotlin.ExperimentalStdlibApi
-fun solve() {
+fun presolve() {
 
 }
 
-var output : PrintWriter = PrintWriter(BufferedOutputStream(if (DEBUG) FileOutputStream("out.log") else System.out))
+@kotlin.ExperimentalStdlibApi
+fun solve() {
+    
+}
+
+var output : PrintWriter = PrintWriter(BufferedOutputStream(if (FILE_IN_OUT) FileOutputStream(FILE_OUT) else System.out))
 
 fun _println(a : Any) = output.println(a)
 fun _println() = output.println()
@@ -33,15 +41,17 @@ fun _nextint() : Int = _nextstring().toInt()
 fun _nextlong() : Long = _nextstring().toLong()
 fun _nextline() : String = br.readLine() ?: ""
 fun _nextstring() : String { while (st.hasMoreElements().not()) st = StringTokenizer(br.readLine() ?: return "", " "); return st.nextToken() }
+fun IntArray.read() : IntArray {for (i in indices) this[i] = _nextint(); return this}
+fun LongArray.read() : LongArray {for (i in indices) this[i] = _nextlong(); return this}
+fun Array<String>.read() : Array<String> {for (i in indices) this[i] = _nextstring(); return this}
 
-fun IntArray.read() : IntArray {for (i in this.indices) this[i] = _nextint(); return this}
-fun LongArray.read() : LongArray {for (i in this.indices) this[i] = _nextlong(); return this}
+fun Array<IntArray>.read() : Array<IntArray> {for (i in indices) for (j in this[i].indices) this[i][j] = _nextint(); return this}
 
-fun Array<IntArray>.read() : Array<IntArray> {for (i in this.indices) {for (j in this[i].indices) this[i][j] = _nextint()}; return this}
+fun IntArray.arrToArr() : Array<Int> {val res = Array<Int>(size) {0}; for (i in indices) res[i] = this[i]; return res}
 
-fun _nextints() : ArrayList<Int> = ArrayList(_nextline().split(" ").map {it -> it.toInt()})
-fun _nextlongs() : ArrayList<Long> = ArrayList(_nextline().split(" ").map {it -> it.toLong()})
+fun _nextints() : ArrayList<Int> = ArrayList(_nextline().split(" ").map {it.toInt()})
+fun _nextlongs() : ArrayList<Long> = ArrayList(_nextline().split(" ").map {it.toLong()})
 fun _nextstrings() : ArrayList<String> = ArrayList(_nextline().split(" "))
 
-val br = BufferedReader(InputStreamReader(if (DEBUG) FileInputStream("in.log") else System.`in`))
+val br = BufferedReader(InputStreamReader(if (FILE_IN_OUT) FileInputStream(FILE_IN) else System.`in`))
 var st = StringTokenizer("")
